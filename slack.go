96360e60e5e5add41fc7fdbd5e880c6a09e66a32
makki0205 <llxo2_5oxll@icloud.com>
2018-11-13 10:01:58 +0900

    .

---
+++ slack.go
@@ -0,0 +1,35 @@
+package log
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/makki0205/config"
+)
+
+var SlackURL = config.Env("slack_url")
+
+func SendSlack(msg string) {
+	payload := map[string]interface{}{
+		"text": msg,
+	}
+	str, err := json.Marshal(payload)
+	if err != nil {
+		return
+	}
+	values := url.Values{}
+	values.Set("payload", string(str))
+
+	req, _ := http.NewRequest(
+		"POST",
+		SlackURL,
+		strings.NewReader(values.Encode()),
+	)
+
+	// Content-Type 設定
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	client.Do(req)
+}
