b496b7f0ffd2bd9b345f338061b2b8573e84b556
makki0205 <llxo2_5oxll@icloud.com>
2018-11-30 10:30:43 +0900

    .

--- log.go
+++ log.go
@@ -38,7 +38,7 @@ func Err(err interface{}, arg ...string) {
 		for _, value := range arg {
 			errs += fmt.Sprintf("[%s]", value)
 		}
-		errs += fmt.Sprintf("\npanic recovered:n%s\n%s\n\n", err, stack(3))
+		errs += fmt.Sprintf("\npanic recovered:n%s\n%s\n\n", err, stack(0))
 		fmt.Println("\n\n\x1b[31m" + errs + reset)
 		SendSlack(errs)
 	}
