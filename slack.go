fec6af98daa95fe19ebaab48a50f96acbe63a7c6
ohtukasyota <syota.jaska@gmail.com>
2019-02-09 11:21:02 +0900

    チャンネル指定でも送れるように

--- slack.go
+++ slack.go
@@ -33,3 +33,27 @@ func SendSlack(msg string) {
 	client := &http.Client{}
 	client.Do(req)
 }
+
+func SendSlack(msg, channel string) {
+	payload := map[string]interface{}{
+		"text": msg,
+		"channel": channel,
+	}
+	str, err := json.Marshal(payload)
+	if err != nil {
+		return
+	}
+	values := url.Values{}
+	values.Set("payload", string(str))
+
+	req, _ := http.NewRequest(
+		"POST",
+		SlackURL,
+		strings.NewReader(values.Encode()),
+	)
+
+	// Content-Type 設定
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	client.Do(req)
+}
